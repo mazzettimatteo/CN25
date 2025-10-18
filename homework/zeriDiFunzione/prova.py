9bd01a16d9697fff338ab28a68951e4465cd3aa1
Matteo Mazzetti <52881504+iammattbunch@users.noreply.github.com>
2025-10-18 15:23:13 +0200

    terminato newton

--- homework/zeriDiFunzione/prova.py
+++ homework/zeriDiFunzione/prova.py
@@ -4,22 +4,16 @@ import matplotlib.pyplot as plt
 
 f1=lambda x: np.log(x+1)-x
 g1=lambda x: np.log(x+1)
-dg1=lambda x: 1/(x+1)
+h1=lambda x: 1/(1/(x+1)-1)
 
 iter=50
 toll=1.e-10
 
 
-def puntoFisso(f,g,dg,x0,t1,t2,N): 
-    #controlli: abs(dg)<1 
-    if(np.abs(dg1(x0))<1):
-        raise ValueError("g non è una contrazione")
-
-
+def puntoFisso(f,g,x0,t1,t2,N):
     cont=0
-    while(np.abs(f(x0))>t1 and cont<N):    
+    while(np.abs(f(x0))>t1 and cont<N): 
         xNew=g(x0)
-        print(f"{xNew}")
         delta=np.abs(x0-xNew)
         if(delta<t2): 
             break
@@ -28,7 +22,7 @@ def puntoFisso(f,g,dg,x0,t1,t2,N):
     return x0
 
 
-x1Star=puntoFisso(f1,g1,dg1,0,toll,toll,iter)
+x1Star=puntoFisso(f1,h1,0.5,toll,toll,iter)
 print(f"Uno zero di f1 è in x={x1Star}")
 f1val=f1(x1Star)
-print(f"Il valore di f1 in x1Star è {f1val}")
+print(f"Il valore di f1 in x1Star è {f1val}")   #errato
