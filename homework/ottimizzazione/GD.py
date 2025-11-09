eb39b8c6a470a0a3cd1d19c3033f111c12dbfa54
Matteo Mazzetti <52881504+iammattbunch@users.noreply.github.com>
2025-11-09 12:08:51 +0100

    creata funz graficoErrori; Fa, Fb terminate

--- homework/ottimizzazione/GD.py
+++ homework/ottimizzazione/GD.py
@@ -1,6 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
+def graph3Diters(x, y, z, traj,f):
+    traj=np.array(traj)
+    fig = plt.figure(figsize=(6, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    surf = ax.plot_surface(x, y, z, cmap='viridis', rstride=1, cstride=1, linewidth=0, antialiased=True)
 
+    zTraj=[f(i) for i in traj]
+    ax.plot3D(traj[:,0], traj[:,1], zTraj, 'r.-', label='GD path')
+    ax.plot3D(traj[-1,0], traj[-1,1], 'bo', label='Minimum') # final point
+
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
+    plt.show()
+def graph2Diters(x, y, z,traj):
+    traj=np.array(traj)
+    fig, ax = plt.subplots(figsize=(6, 6))
+    contours = ax.contour(x, y, z, levels=20, cmap='viridis')
+    ax.clabel(contours, inline=True, fontsize=8)
+
+    ax.plot(traj[:,0], traj[:,1], 'r.-', label='GD path')  # red line with dots
+    ax.plot(traj[-1,0], traj[-1,1], 'bo', label='Minimum') # final point
+
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.legend()
+    ax.axis('equal')
+    plt.show()
 
 def backtrackingFun(f,df,x,alpha=1,rho=0.5,c=1.e-4):
     while f(x-alpha*df(x))>f(x)-c*alpha*(np.linalg.norm(df(x))**2):
@@ -11,6 +38,7 @@ def GD(f,df,x0,alpha,maxIt,fToll,xToll,alphaConst):    #f:R^n->R
     cont=0
     n=len(x0)
     dfNorm=np.linalg.norm(df(x0))
+    allIters=[np.array(x0)]
     while cont<maxIt and dfNorm>fToll:
         p=-df(x0)
         alpha=alpha if alphaConst else backtrackingFun(f,df,x0)#backtracking o costante
@@ -18,6 +46,21 @@ def GD(f,df,x0,alpha,maxIt,fToll,xToll,alphaConst):    #f:R^n->R
         if(np.linalg.norm(xNew-x0)<xToll):
             break
         x0=xNew
+        allIters.append(x0.copy())
         dfNorm=np.linalg.norm(df(x0))
         cont+=1
-    return x0,f(x0),cont    #p.to di minimo, val di minimo, iterazioni fatte
+    return x0,f(x0),cont,alphaConst,alpha,allIters    #p.to di minimo, val di minimo, iterazioni fatte
+
+
+
+
+maxIt=10000
+fT=1.e-6
+xT=1.e-5
+
+(xMin, val, iters,alphaWasConst,a,allIt)=GD(funB, gradB, (0,2), 0.001, maxIt, fT, xT, False)
+res=(xMin, val, iters,alphaWasConst,a)
+print(f"{res}")
+
+graph2Diters(XB, YB, ZB, allIt)
+graph3Diters(XB, YB, ZB, allIt, funB)
--- homework/ottimizzazione/disegnaSuperfici.py
+++ homework/ottimizzazione/disegnaSuperfici.py
@@ -8,23 +8,21 @@ def make3Dgraph(x, y, z):
     fig = plt.figure(figsize=(6, 6))
     ax = fig.add_subplot(111, projection='3d')
     surf = ax.plot_surface(x, y, z, cmap='viridis', rstride=1, cstride=1, linewidth=0, antialiased=True)
-    ax.set_title("Superficie 3D: z = f(x, y)")
+    #ax.set_title("Superficie 3D: z = f(x, y)")
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.set_zlabel("z")
     plt.show()
-
 def make2Dgraph(x, y, z):
     """Draws a 2D contour plot (level curves) for z = f(x, y)."""
     fig, ax = plt.subplots(figsize=(6, 6))
     contours = ax.contour(x, y, z, levels=20, cmap='viridis')
     ax.clabel(contours, inline=True, fontsize=8)
-    ax.set_title("Proiezione 2D (curve di livello)")
+    #ax.set_title("Proiezione 2D (curve di livello)")
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.axis('equal')
     plt.show()
-
 def graph3Dmin(x, y, z,xMin,value):
     """Draws a 3D surface plot for z = f(x, y)."""
     fig = plt.figure(figsize=(6, 6))
@@ -32,39 +30,71 @@ def graph3Dmin(x, y, z,xMin,value):
     surf = ax.plot_surface(x, y, z, cmap='viridis', rstride=1, cstride=1, linewidth=0, antialiased=True)
     ax.plot3D(xMin[0],xMin[1],value,'o',color='red')
 
-    ax.set_title("Superficie 3D: z = f(x, y)")
+    #ax.set_title("Superficie 3D: z = f(x, y)")
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.set_zlabel("z")
-
-
 def graph2Dmin(x, y, z,xMin,value):
     """Draws a 2D contour plot (level curves) for z = f(x, y)."""
     fig, ax = plt.subplots(figsize=(6, 6))
     contours = ax.contour(x, y, z, levels=20, cmap='viridis')
     ax.clabel(contours, inline=True, fontsize=8)
-    ax.set_title("Proiezione 2D (curve di livello)")
+    #ax.set_title("Proiezione 2D (curve di livello)")
     ax.plot(xMin[0],xMin[1],'o',color='red')
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.axis('equal')
 
-
-x = np.linspace(2, 8, 200)
-y = np.linspace(-1, 5, 200)
-X, Y = np.meshgrid(x, y)
-Z=(X-5)**2+(Y-2)**2
+#---------------------------------------------funz. A
+"""xA = np.linspace(2, 8, 200)
+yA = np.linspace(-1, 5, 200)
+XA, YA = np.meshgrid(xA, yA)
+ZA=(XA-5)**2+(YA-2)**2
 #lets convert that int a real function
-def fun(vector):
+def funA(vector):
     x,y=vector
     return (x-5)**2+(y-2)**2
-def grad(vector):
+def gradA(vector):
     x,y=vector
     return np.array([2*(x-5),2*(y-2)])
+"""
+#---------------------------------------------funz. B
+"""xB = np.linspace(-2, 4, 200)
+yB = np.linspace(-2, 4, 200)
+XB, YB = np.meshgrid(xB, yB)
+ZB=(1-XB)**2 +100*((YB-(XB**2))**2)
+def funB(vector):
+    x,y=vector
+    return ((1-x)**2) +100*((y-(x**2))**2)
+
+def gradB(vector):
+    x,y=vector
+    grad=np.array([-2*(1-x)-400*x*(y-x**2) , 200*(y-x**2)])
+    return grad"""
+#----------------------------------------------funz D
+"""def funAndGradD(x,n):
+    #n must be x lenght
+    A=np.random.random((n,n))
+    oneVect=np.ones(n)
+    b=A @ oneVect
+    normContent=(A @ x)-b
+
+    funz=0.5*(np.linalg.norm(normContent)**2)
+    grad=A.T @ ((A @ x) - b)
+
+    return (funz,grad)"""
+#-----------------------------------------funzF
+"""def funF(x,n):
+    #x must contasin only positive nums
+    i=np.arange(1,n)
+    funz=sum((x-i)**2) - sum(np.log((x-i)))
+def gradF(x,n):
+    i=np.arange(1,n)
+    return 2*(x-i)-(1/x)
+def stocGradF(x,n):
+    """
 
 
-make3Dgraph(X,Y,fun(X,Y))
-make2Dgraph(X,Y,fun(X,Y))
-#graph2Dmin(X,Y,fun([X,Y]),xStar,val)
-#graph3Dmin(X,Y,fun([X,Y]),xStar,val)
+#graph2Dmin(XA,YA,fun([XA,YA]),xStar,val)
+#graph3Dmin(XA,YA,fun([XA,YA]),xStar,val)
 plt.show()
