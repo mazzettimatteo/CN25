8a99e049614ac8523e16f6c4df17d4241d733ff5
Matteo Mazzetti <52881504+iammattbunch@users.noreply.github.com>
2025-12-20 12:12:54 +0100

    HW3: aggiunto dataset

--- homework/analisiDati/simpleReg.py
+++ homework/analisiDati/simpleReg.py
@@ -31,16 +31,53 @@ def f(x, alpha):
         y = y + alpha[i] * x ** i
     return y
 
-dataSet=pd.read_csv("dataSets/Salary_Data.csv")
+####################################
 
-print(f"Head dataset: \n{dataSet.head()}")
+dataSet1=pd.read_csv("dataSets/SalaryDataset.csv")
+
+print(f"Head dataset: \n{dataSet1.head()}")
 #print(f"Info datset: \n {dataSet.info()}")
-print(f"Descrizione dataset:\n{dataSet.describe()}")
+print(f"Descrizione dataset:\n{dataSet1.describe()}")
 
-xRaw=dataSet["YearsExperience"].values
-yRaw=dataSet["Salary"].values
+xRaw=dataSet1["YearsExperience"].values
+yRaw=dataSet1["Salary"].values
 
-print(f"Numero di righe:{yRaw.shape[0]}")
+print(f"Numero di righe Salary Dataset:{yRaw.shape[0]}")
+
+
+d=3#-----------------------------------------------------------PARAM DA CAMBIARE
+
+X=vandermonde(xRaw,d)
+
+alphaSVD=SVD(X,yRaw,d)
+
+xMin=xRaw.min()
+xMax=xRaw.max()
+xx=np.linspace(xMin-1, xMax+1,10000)
+yy=f(xx,alphaSVD)
+
+
+plt.plot(xRaw, yRaw,'r.')
+plt.plot(xx,yy,'g')
+plt.xlabel("Esperienza (Y)")
+plt.ylabel("Salario (USD)")
+plt.grid()
+plt.show()
+###########################################################################
+dataSet2=pd.read_csv("dataSets/HousePrice.csv")
+
+#rimuovo le colonne che non mi interessano, tengo solo: Square_Footage, House_Price
+dataSet2.pop("Num_Bedrooms")
+dataSet2.pop("Num_Bathrooms")
+dataSet2.pop("Year_Built")
+dataSet2.pop("Lot_Size")
+dataSet2.pop("Garage_Size")
+dataSet2.pop("Neighborhood_Quality")
+
+xRaw=dataSet2["Square_Footage"].values
+yRaw=dataSet2["House_Price"].values
+
+print(f"Numero di righe Housing Dataset:{yRaw.shape[0]}")
 
 
 d=5#-----------------------------------------------------------PARAM DA CAMBIARE
@@ -54,9 +91,10 @@ xMax=xRaw.max()
 xx=np.linspace(xMin-1, xMax+1,10000)
 yy=f(xx,alphaSVD)
 
+
+plt.plot(xRaw, yRaw,'r.')
 plt.plot(xx,yy,'g')
-plt.plot(xRaw, yRaw,'ro')
-plt.xlabel("Esperienza (Y)")
-plt.ylabel("Salario (USD)")
+plt.xlabel("Dimensioni (ft^2)")
+plt.ylabel("Prezzo (USD)")
 plt.grid()
 plt.show()
